fee301191c73065fcf9bef9fd2b1709b20c2522c
=DamjanRadev <radevdamjan@gmail.com>
2019-07-22 16:25:36 +0200

    Hello name react example app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import HelloMessage from './components/helloMessage';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div> 
+        <HelloMessage name="DaGlupcomjan" />
+      </div>
+    );
+  }
+  
+}
+
+export default App;
